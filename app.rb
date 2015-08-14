7d524b33ea891c1bb5f9d9355ab58f8c0af08ae0
Christoph Pirringer <christoph.pirringer@gmail.com>
2015-08-14 10:28:58 -0700

    add functionality to the form fo add words as well as to the page that lists existing ones

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ require("./lib/definition")
 get("/") do
   erb(:index)
 end
+
+get('/words') do
+  @words = Word.all()
+  erb(:words)
+end
+
+get('/words/new') do
+  erb(:words_form)
+end
+
+post("/words") do
+  word = params.fetch("word")
+  Word.new(word).save()
+  @words = Word.all()
+  erb(:success)
+end
